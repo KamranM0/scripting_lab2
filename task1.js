2b4cc638244da0a060fb259434d50f49bd948028
KamranM0 <kamran.mammadzada04@gmail.com>
2024-11-03 12:24:45 +0400

    first commit

---
+++ task1.js
@@ -0,0 +1,18 @@
+//task1
+
+function setCancellableInterval(callback, delay, ...params) {
+  const cancelFunction = (interval_id) => clearInterval(interval_id);
+  const interval = setInterval(() => callback(...params), delay);
+  return () => cancelFunction(interval);
+}
+var i = 0;
+const cancel = setCancellableInterval(
+  (a, b) => {
+    console.log(a);
+    console.log(b);
+  },
+  1000,
+  3,
+  5
+);
+setTimeout(cancel, 5000);
---
+++ task3.js
@@ -0,0 +1,13 @@
+//task3
+
+const sleep = (delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, delay);
+  });
+};
+const greeting = async () => {
+  console.log("hello");
+  await sleep(1000);
+  console.log("bye");
+};
+greeting();
